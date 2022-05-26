ddcc02371b68d331618340e9aa2f11ccd4093322
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-05-26 19:30:41 -0400

    Condense down

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # github-watch-all-repos
 Watch all repositories belonging to an individual user/organization.
+
+## Usage
+
+<!-- usage -->
+```sh-session
+$ npm install -g github-watch-all-repos
+$ github-watch-all-repos COMMAND
+running command...
+$ github-watch-all-repos (--version)
+github-watch-all-repos/1.0.0 darwin-x64 node-v18.2.0
+$ github-watch-all-repos --help [COMMAND]
+USAGE
+  $ github-watch-all-repos COMMAND
+...
+```
+<!-- usagestop -->
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export {run} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
+
+export default class GithubWatchAllRepos extends Command {
+  static description = 'Watch all repositories belonging to an individual user/organization.'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: Flags.version({char: 'v'}),
+    help: Flags.help({char: 'h'}),
+    user: Flags.boolean({char: 'u', description: 'Username represents an user.', default: false}),
+    organization: Flags.boolean({char: 'o', description: 'Username represents an organization.', default: false}),
+  }
+
+  static args = [{name: 'username', description: 'The username of the user/organization to watch.', required: true}]
+
+  async run() {
+    const {args, flags} = await this.parse(GithubWatchAllRepos)
+
+    if (flags.user == flags.organization){
+      this.error('You must only specify one of --user/--organization.')
+    }
+  }
+}
+
+GithubWatchAllRepos.run()
